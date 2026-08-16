222285deb58c7e96b4e7935d09039f52c80a669a
ed3c <mcnum01@gmail.com>
2026-08-16 17:47:56 +0800

    use production site start script in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     screenshot: "off"
   },
   webServer: {
-    command: "pnpm --filter @website-design-compiler/site start --hostname 127.0.0.1 --port 3000",
+    command: "pnpm --filter @website-design-compiler/site start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
